49eebe47a74f4358020488c3d097b4baab1c496d
v5al <udamvisal18@kit.edu.kh>
2020-10-01 04:08:40 +0700

    added static folder and prepare SPA

--- server/index.js
+++ server/index.js
@@ -9,6 +9,15 @@ app.use(cors());
 
 const tasklist = require('./routes/api/tasklist');
 
+//Handle Production
+if(process.env.NODE_ENV === 'production'){
+    //Static Folder
+    app.use(express.static(__dirname + '/public/'))
+
+    //Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 app.use('/api/tasklist', tasklist);
 
 const port = process.env.PORT || 5000;
