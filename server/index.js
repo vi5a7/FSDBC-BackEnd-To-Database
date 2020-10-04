732b9b9fd328f40f1d43c4c060c64471b065a25a
v5al <udamvisal18@kit.edu.kh>
2020-10-04 21:07:41 +0700

    edit backend

--- server/index.js
+++ server/index.js
@@ -5,9 +5,11 @@ const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 const tasklist = require('./routes/api/tasklist');
+app.use('/api/tasklist', tasklist);
 
 //Handle Production
 if(process.env.NODE_ENV === 'production'){
@@ -17,8 +19,6 @@ if(process.env.NODE_ENV === 'production'){
     //Handle SPA
     app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
-
-app.use('/api/tasklist', tasklist);
 
 const port = process.env.PORT || 5000;
 
--- server/routes/api/tasklist.js
+++ server/routes/api/tasklist.js
@@ -3,6 +3,11 @@ const mongodb = require('mongodb')
 
 const router = express.Router()
 
+async function loadTasksCollection(){
+    const client = await mongodb.MongoClient.connect("mongodb+srv://visal:1234@visalclouddb.k7ikj.gcp.mongodb.net/my_tasklist?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
+    return client.db('my_tasklist').collection("mytasks")
+}
+
 //Get Tasks
 router.get('/', async (req, res) => {
     const tasklist = await loadTasksCollection();
@@ -12,7 +17,6 @@ router.get('/', async (req, res) => {
 //Add Task
 router.post('/', async(req, res) => {
     const tasklist = await loadTasksCollection();
-    console.log(req.body.task)
     await tasklist.insertOne({
         task: req.body.task,
         dateCreated: new Date()
@@ -26,10 +30,5 @@ router.delete('/:id', async(req, res) => {
     await tasklist.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
     res.status(200).send()
 })
-
-async function loadTasksCollection(){
-    const client = await mongodb.MongoClient.connect("mongodb+srv://visal:1234@visalclouddb.k7ikj.gcp.mongodb.net/my_tasklist?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true})
-    return client.db('my_tasklist').collection("mytasks")
-}
 
 module.exports = router
